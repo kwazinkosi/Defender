f911247d334ebe51e124acbca2f516f878653f01
Kwazinkosi Zwane <kwazinkosizwane@gmail.com>
2023-09-10 16:41:27 +0200

    Updated Context class

--- game-source-code/Context.cpp
+++ game-source-code/Context.cpp
@@ -2,7 +2,14 @@
 
 Context::Context() : mWindow(sf::VideoMode(800, 600), "Defender Shooter", sf::Style::Close)
 {
+    mBackgrounds = std::make_unique<BackgroundsHolder>();
+    mFonts = std::make_unique<FontsHolder>();
+    mTextures = std::make_unique<TexturesHolder>();
 
+    if (!mBackgrounds || !mFonts || !mTextures)
+    {
+        throw ContextInitializationException{};
+    }
 }
 
 Context::~Context()
--- game-source-code/Context.hpp
+++ game-source-code/Context.hpp
@@ -7,12 +7,19 @@
 #include <SFML/System.hpp>
 #include <memory>
 
+typedef ResourceManager<sf::Texture, Textures> TexturesHolder; // Typedef for the Textures resource manager.
+typedef ResourceManager<sf::Texture, Backgrounds> BackgroundsHolder; // Typedef for the Backgrounds resource manager.
+typedef ResourceManager<sf::Font, Fonts> FontsHolder; // Typedef for the Fonts resource manager.
+
+class ContextInitializationException{};
 class Context
 {
     public:
         Context();
         ~Context();
-
+        std::unique_ptr<BackgroundsHolder> mBackgrounds; // Unique pointer to the Backgrounds resource manager. 
+        std::unique_ptr<TexturesHolder> mTextures; // Unique pointer to the Textures resource manager.
+        std::unique_ptr<FontsHolder> mFonts;
         sf::RenderWindow mWindow;
 };
 #endif // Context_HPP
