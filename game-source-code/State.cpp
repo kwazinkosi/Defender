8fa9490b7e6f673e46ad45ab87934d81a6b9c94c
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-10 16:16:03 +0200

    State class cpp file implementation

--- game-source-code/State.cpp
+++ game-source-code/State.cpp
@@ -1,1 +1,35 @@
 #include "State.hpp"
+
+State::State(StateStack &stack, Context &context) : mStack(&stack) , mContext(&context)
+{
+
+}
+
+State::~State()
+{
+}
+/**
+ * @brief This is a function that is called when the state is pushed onto the stack.
+ * 
+ * @param stateID  The ID of the state (enum class States::ID)
+ */
+void State::requestStackPush(States stateID)
+{
+    mStack->pushState(stateID);
+}
+/**
+ * @brief This function is called when the state is popped from the stack.
+ * 
+ */
+void State::requestStackPop()
+{
+    mStack->popState();
+}
+/**
+ * @brief This function is called when the state stack is cleared.
+ * 
+ */
+void State::requestStateClear()
+{
+    mStack->clearStates(); //clears all the states from the stack
+}
