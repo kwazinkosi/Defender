57416596e9fd6395045b40585deb65e10b580889
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-10 16:31:11 +0200

    StateStack class cpp file second update

--- game-source-code/StateStack.cpp
+++ game-source-code/StateStack.cpp
@@ -65,3 +65,53 @@ void StateStack::handleEvent(const sf::Event &event, sf::RenderWindow &window)
     // This means that the state's handleEvent() function has returned true, so the event is handled.
     applyPendingChanges();
 }
+/**
+ * @brief Pushes a new state to the stack.
+ *
+ * - This function pushes a new state to the stack by adding the push action to the mPendingList vector.
+ */
+void StateStack:: pushState(States stateID)
+{
+    PendingChange change;
+    change.action = Action::Push;
+    change.stateID = stateID;
+    mPendingList.push_back(change);
+}
+
+/**
+ * @brief Pops the top state from the stack.
+ *
+ * - popState() pops the top state from the stack by adding the pop action to the mPendingList vector.
+ */
+void StateStack::popState()
+{
+    if (mStack.empty())
+    {
+        throw StateStackException{};
+    }
+    PendingChange change;
+    change.action = Action::Pop;
+    mPendingList.push_back(change);
+}
+/**
+ * @brief Clears all the states from the stack.
+ *
+ * - clearStates() clears all the states from the stack by adding the clear action to the mPendingList vector.
+ */
+void StateStack::clearStates()
+{
+    PendingChange change;
+    change.action = Action::Clear;
+    mPendingList.push_back(change);
+}
+
+/**
+ * @brief Checks if the stack is empty.
+ *
+ * @return true If the stack vector is empty, false otherwise.
+ */
+bool StateStack::isEmpty() const
+{
+    // Returns true if the vector is empty, false otherwise.
+    return mStack.empty();
+}
