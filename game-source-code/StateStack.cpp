93bf208efbbbc51b8ab27acf6720d2da209a1c61
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-10 16:29:43 +0200

    StateStack class cpp file update

--- game-source-code/StateStack.cpp
+++ game-source-code/StateStack.cpp
@@ -28,3 +28,40 @@ void StateStack::update(sf::Time dt)
     }
     applyPendingChanges();
 }
+/**
+ * @brief Draws the game state.
+ *
+ * - This function draws the game state by iterating through each state in the stack and drawing it.
+ */
+void StateStack::draw(sf::RenderWindow &window)
+{
+    // Draws the states in the stack from the bottom to the top.
+    for (State::Ptr &state : mStack)
+    {
+        state->draw(window);
+    }
+}
+/**
+ * @brief Handles keyboard input for the game state.
+ *
+ * - The function handles keyboard input by iterating through each state in the stack and handling the event.
+ * - The active state is the state at the top of the stack, and it is the only state that can handle events unless it returns false.
+ * - The function also applies any pending changes to the stack if there are any.
+ *
+ * @param event
+ */
+void StateStack::handleEvent(const sf::Event &event, sf::RenderWindow &window)
+{
+    
+    for (auto itr = mStack.rbegin(); itr != mStack.rend(); ++itr)
+    {
+        // If the state returns false, then it means that it doesn't want the states below it to handle the event.
+        if (!(*itr)->handleEvent(event, window)) // This handleEvent() function is the one that is defined in the State class.
+        {
+            // This means that the state's handleEvent() function has returned false, so the event is not handled.
+            break;
+        }
+    }
+    // This means that the state's handleEvent() function has returned true, so the event is handled.
+    applyPendingChanges();
+}
