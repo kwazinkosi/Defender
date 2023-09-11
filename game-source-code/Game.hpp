f2e46db618f5a3946cd056febf3fd004456f915c
Kwazinkosi Zwane <kwazinkosizwane@gmail.com>
2023-09-11 12:48:48 +0200

    Updated Game class

--- game-source-code/Game.hpp
+++ game-source-code/Game.hpp
@@ -4,6 +4,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
+#include "StateStack.hpp"
 #include "Context.hpp"
 class Game
 {
@@ -13,11 +14,13 @@ class Game
         void runGame();
 
     private:
+        void registerStates();
         void processEvents();
         void update(sf::Time deltaTime);
         void render();
 
         const sf::Time mTimePerFrame = sf::seconds(1.f/60.f); // The time per frame, used to update the game state.
+        StateStack mStateStack;
         Context mContext; // The context of the game (e.g., window, resource managers).
 };
 
