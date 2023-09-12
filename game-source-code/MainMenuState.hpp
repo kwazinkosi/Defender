a2602ad68eb34bdbe97cb8480f891ede7763d57f
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-12 10:03:29 +0200

    MainMenuState header file implementation

---
+++ game-source-code/MainMenuState.hpp
@@ -0,0 +1,34 @@
+#ifndef MAINMENUSTATE_HPP
+#define MAINMENUSTATE_HPP
+
+#include "State.hpp"
+#include "TextFormater.hpp"
+#include "StateStack.hpp"
+
+enum class MenuOptions
+{
+   Play, Help, Options, Exit
+};
+class MainMenuState : public State
+{
+    public:
+        MainMenuState(StateStack &stack, Context &context);
+
+        virtual void draw(sf::RenderWindow &window);
+        virtual bool update(sf::Time dt);   
+        virtual bool handleEvent(const sf::Event &event, sf::RenderWindow &window);
+        virtual std::string getStateID() const;
+        
+        void setupMenuOptions(); //To fill the menu options vector
+        void updateOptionText(); //To update the text of the menu options
+        void drawMenuOptions(sf::RenderWindow &window); //To draw the menu options
+
+    private:
+        TextFormater mTextFormater; //To format the text of the menu options
+        sf::Sprite mBackgroundSprite;
+        sf::Text mText;
+        std::vector<sf::Text> mMenuOptions; //To store the menu options
+        std::vector<sf::Text>::iterator mMenuOptionsIterator; 
+};
+
+#endif // MAINMENUSTATE_HPP
