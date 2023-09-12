eba5526c437f7383174fd433e27e500957de2eed
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-12 09:29:13 +0200

    SplashState and TextFormatter header files

---
+++ game-source-code/SplashState.hpp
@@ -0,0 +1,31 @@
+#ifndef SPLASHSTATE_HPP
+#define SPLASHSTATE_HPP
+
+#include "State.hpp"
+#include "StateStack.hpp"
+#include "TextFormater.hpp"
+
+class SplashState : public State
+{
+    public:
+        SplashState(StateStack &stack, Context &context);
+
+        //virtual because we are overriding the virtual functions in the State class
+        virtual void draw(sf::RenderWindow &window);
+        virtual bool update(sf::Time dt);
+        virtual bool handleEvent(const sf::Event &event, sf::RenderWindow &window);
+        virtual std::string getStateID() const;
+
+    private:
+        bool mShowText;
+        sf::Sprite mBackgroundSprite;
+        //sf::Sprite mKeypadSprite;
+        sf::Texture mTexture;
+        sf::Text mText;
+        sf::Time mElapsedTime;
+
+        TextFormater mTextFormater;
+        
+};
+
+#endif // SPLASHSTATE_HPP
