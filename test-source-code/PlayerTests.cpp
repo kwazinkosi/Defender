16f8e6e4e9e3db6bc11ed595d0713ea4dd9bfff3
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-14 16:23:23 +0200

    PlayerTests implemented, player initialisation

--- test-source-code/PlayerTests.cpp
+++ test-source-code/PlayerTests.cpp
@@ -35,3 +35,55 @@ TEST_CASE("Context initialization") {
     CHECK(context.mFonts != nullptr);
     CHECK(context.mTextures != nullptr);
 }
+//Testing LoadingState
+TEST_CASE("ResourceManager - getResourceById") {
+    ResourceManager<sf::Texture, Textures> textureManager; // Create a texture manager
+    
+    // Add textures to the manager
+    textureManager.load(Textures::Player, "resources/textures/spaceship.png");
+    
+    // Test getting an existing texture
+    CHECK_NOTHROW(textureManager.getResourceById(Textures::Player));
+    
+    // Test getting a non-existing texture
+    //CHECK_THROWS_AS(textureManager.getResourceById(Textures::NonExistent), std::runtime_error);
+}
+//Testing StateStack functionality
+
+TEST_CASE("StateStack is empty upon creation") {
+    StateStack stack;
+    
+    CHECK(stack.isEmpty());
+}
+
+TEST_CASE("Poping an empty stack throws an exception") {
+    StateStack stack;
+   
+    // Attempt to pop an empty stack (expecting an exception)
+    CHECK_THROWS_AS(stack.popState(), StateStackException);
+}
+
+TEST_CASE("StateStack - Clear States") {
+    StateStack stack;
+    Context context;
+    
+    // Push multiple states
+    stack.pushState(States::MainMenuState);
+    stack.pushState(States::GameState);
+    stack.pushState(States::PauseState);
+    
+    // Clear all states and check if the stack is empty
+    stack.clearStates();
+    CHECK(stack.isEmpty());
+}
+
+TEST_CASE("Player initialization and getters")
+{
+
+    // CHECK(player.mPlayer.getRadius().== 30.f); // Radius * 2
+    // CHECK(player.getBoundingBox().height == 30.f);
+    CHECK(player.getPosition() == sf::Vector2f(100.f, 100.f));
+    CHECK(player.getCollisionType() == "Player");
+    CHECK(player.getCollisionResponse() == "Player");
+    CHECK(player.isStatic() == false);
+}
