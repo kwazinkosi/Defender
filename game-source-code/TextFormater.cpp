63af0370cbc00bb336e0f171f5c234198465ae95
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-19 13:12:16 +0200

    TextFormatter update

--- game-source-code/TextFormater.cpp
+++ game-source-code/TextFormater.cpp
@@ -30,3 +30,62 @@ void TextFormater::drawText(sf::RenderWindow &window)
     window.draw(mText);
 }
 
+void TextFormater::setFont(const std::string &fontPath)
+{
+    if (!mFont.loadFromFile(fontPath))
+    {
+        throw std::runtime_error("Font not found");
+    }
+
+    mText.setFont(mFont);
+
+}
+
+void TextFormater::setString(const std::string &text)
+{
+    mText.setString(text);
+}
+
+void TextFormater::setPosition(const sf::Vector2f &position)
+{
+    mText.setPosition(position);
+}
+
+void TextFormater::centerOrigin(sf::Text &text)
+{
+    sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.left + textRect.width/2.0f, textRect.top  + textRect.height/2.0f);
+
+}
+
+void TextFormater::setFillColor(const sf::Color &color)
+{
+    mText.setFillColor(color);
+}
+
+void TextFormater::setStyle(sf::Text::Style style)
+{
+    mText.setStyle(style);
+}
+
+void TextFormater::setCharacterSize(const unsigned int &size)
+{
+    mText.setCharacterSize(size);
+}
+
+void TextFormater::setOutlineColor(const sf::Color &color)
+{
+    mText.setOutlineColor(color);
+}
+
+void TextFormater::setOutlineThickness(const float &thickness)
+{
+    mText.setOutlineThickness(thickness);
+}
+
+void TextFormater::setLetterSpacing(const float &spacingFactor)
+{
+    mText.setLetterSpacing(spacingFactor);
+}
+
+
--- game-source-code/TextFormater.hpp
+++ game-source-code/TextFormater.hpp
@@ -12,7 +12,16 @@ class TextFormater{
         ~TextFormater();
         sf::Text formatText(const std::string &text, const sf::Vector2f &position, const sf::Color &color, sf::Text::Style, const unsigned int &size);
         void drawText(sf::RenderWindow &window);
-       
+        void setFont(const std::string &fontPath);
+        void setString(const std::string &text);
+        void setPosition(const sf::Vector2f &position);
+        void centerOrigin(sf::Text &text);
+        void setFillColor(const sf::Color &color);
+        void setStyle(sf::Text::Style style);
+        void setCharacterSize(const unsigned int &size);
+        void setOutlineColor(const sf::Color &color);
+        void setOutlineThickness(const float &thickness);
+        void setLetterSpacing(const float &spacing);
     private:
         sf::Text mText;
         sf::Font mFont;
