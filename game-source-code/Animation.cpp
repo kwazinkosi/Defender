35ea93ccdb1961f3c8dc65f2a92a5f59effba521
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-22 19:11:40 +0200

    Private members declaration

--- game-source-code/Animation.cpp
+++ game-source-code/Animation.cpp
@@ -7,3 +7,11 @@ Animation::Animation()
 Animation::~Animation()
 {
 }
+
+void Animation::update(sf::Time deltaTime)
+{
+}
+
+void Animation::draw(sf::RenderTarget &target)
+{
+}
--- game-source-code/Animation.hpp
+++ game-source-code/Animation.hpp
@@ -12,8 +12,19 @@ class Animation
     public:
         Animation();
         ~Animation();
+        void update(sf::Time deltaTime);
+        void draw(sf::RenderTarget& target);
 
     private:
+        sf::Sprite mSprite;
+        sf::Vector2i mFrameStart;
+        sf::Vector2i mFrameSize;
+        std::size_t mNumFrames;
+        std::size_t mCurrentFrame;
+        sf::Time mDuration;  // How long the animation lasts
+        sf::Time mElapsedTime;
+        sf::Vector2f mPosition;
+        bool mRepeat;
 
 };
 #endif
