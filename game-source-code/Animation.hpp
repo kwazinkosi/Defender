0510eb747133f6794b8c9ceb7db23699df718631
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-22 18:51:34 +0200

    Creating Animation class

---
+++ game-source-code/Animation.hpp
@@ -0,0 +1,19 @@
+#ifndef ANIMATION_HPP
+#define ANIMATION_HPP
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+
+class Animation
+{
+    public:
+        Animation();
+        ~Animation();
+
+    private:
+
+};
+#endif
