27431f8ca2c760f6cc7408f5781516275bc768b0
Kwazinkosi Zwane <kwazinkosizwane@gmail.com>
2023-09-22 18:41:07 +0200

    Created a command struct and Commandueue class

--- game-source-code/CommandQueue.hpp
+++ game-source-code/CommandQueue.hpp
@@ -8,6 +8,41 @@
 #include <queue>
 #include <functional>
 
+enum class Action
+{
+    Idle,
+    MoveLeft,
+    MoveRight,
+    MoveUp,
+    MoveDown,
+    Shoot,
+    FlipShip,
+    ActionCount
+};
+enum class Category
+{
+    Player,
+    Enemy,
+    Projectile,
+    TypeCount
+};
+struct Command
+{
+    Command() :
+        category(Category::Player),
+        action(Action::Idle)
+    {
+    }
+    Command(Action ac, Category cat) : // Change 'category' to 'cat' to avoid naming conflict
+        category(cat),                // Use 'cat' here
+        action(ac)
+    {
+    }
+
+    Category category;
+    Action action;
+};
+
 class CommandQueue
 {
     public:
