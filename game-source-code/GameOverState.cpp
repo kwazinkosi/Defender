6cffe3651d306cc9fe13db133558895029e593cd
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-23 22:53:21 +0200

    GameOverState class functions implementations

--- game-source-code/GameOverState.cpp
+++ game-source-code/GameOverState.cpp
@@ -3,3 +3,30 @@
 GameOverState::GameOverState(StateStack& stack, Context &context)
 {
 }
+
+void GameOverState::draw(sf::RenderWindow &window)
+{
+}
+
+bool GameOverState::update(sf::Time dt)
+{
+    return false;
+}
+
+bool GameOverState::handleEvent(const sf::Event &event, sf::RenderWindow &window)
+{
+    return false;
+}
+
+void GameOverState::handleRealtimeInput(sf::RenderWindow &window)
+{
+}
+
+std::string GameOverState::getStateID() const
+{
+    return std::string();
+}
+
+void GameOverState::onActivate()
+{
+}
--- game-source-code/GameOverState.hpp
+++ game-source-code/GameOverState.hpp
@@ -9,6 +9,12 @@ class GameOverState : public State
 {
     public:
         GameOverState(StateStack& stack, Context &context);
+        virtual void draw(sf::RenderWindow &window);
+        virtual bool update(sf::Time dt);
+        virtual bool handleEvent(const sf::Event &event, sf::RenderWindow &window);
+        virtual void handleRealtimeInput(sf::RenderWindow &window);
+        virtual std::string getStateID() const;
+        void onActivate();
 
     private:
         Context* mContext;
