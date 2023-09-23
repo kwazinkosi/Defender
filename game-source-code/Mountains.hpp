f11f03c3a2ee91dce507c69e796cc693d71954c0
Kwazinkosi Zwane <kwazinkosizwane@gmail.com>
2023-09-24 00:11:57 +0200

    Declaration of Mountains class

---
+++ game-source-code/Mountains.hpp
@@ -0,0 +1,34 @@
+#ifndef MOUNTAINS_HPP
+#define MOUNTAINS_HPP
+
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+#include "Context.hpp"
+
+class Mountains
+{
+    public:
+        Mountains(Context &context, bool isLeft, float scrollSpeed, float posY);
+
+        void update(sf::Time deltaTime);
+        void draw(sf::RenderTarget& window);
+        void initMountains();
+        sf::FloatRect getMountainBounds() const;
+
+    private:
+        sf::View *mWorldView; 
+        sf::Sprite mMountainSprite;
+        sf::Texture mMountainTexture;
+        sf::FloatRect mMountainBounds;
+        std::vector<sf::Sprite> mMountains;
+        float mScrollSpeed;
+        bool mIsLeftScrolling;
+        float mPosY;
+        float viewLeft;
+        float viewRight;
+};
+#endif // #MOUNTAINS_HPP
