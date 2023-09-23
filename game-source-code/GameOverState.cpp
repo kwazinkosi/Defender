490d4aa4f2bca6de2682d2a2e2d7ea7bfef9401c
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-23 22:32:25 +0200

    GameOverState class creation

---
+++ game-source-code/GameOverState.cpp
@@ -0,0 +1,5 @@
+#include "GameOverState.hpp"
+
+GameOverState::GameOverState()
+{
+}
---
+++ game-source-code/GameOverState.hpp
@@ -0,0 +1,18 @@
+#ifndef GAMEOVERSTATE_HPP
+#define GAMEOVERSTATE_HPP
+
+#include <memory>
+#include "State.hpp"
+#include "TextFormater.hpp"
+
+class GameOverState : public State
+{
+    public:
+        GameOverState();
+
+    private:
+        
+
+};
+#endif
+
