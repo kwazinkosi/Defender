00955565b6adfde06c8576c5f006fb3b5d0ca4bf
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-24 13:48:33 +0200

    StarGenerator class functions to be defined

--- game-source-code/StarGenerator.cpp
+++ game-source-code/StarGenerator.cpp
@@ -3,3 +3,44 @@
 StarGenerator::StarGenerator(Context &context)
 {
 }
+void StarGenerator::update(sf::Time deltaTime)
+{
+}
+void StarGenerator::generateStars(int staNum)
+{
+}
+
+void StarGenerator::moveStars(sf::Time deltaTime)
+{
+}
+
+sf::Color StarGenerator::generateStarColor(StarSpectralType spectralType)
+{
+    return sf::Color();
+}
+
+sf::CircleShape StarGenerator::generateStarSize(float mass)
+{
+    return sf::CircleShape();
+}
+
+sf::Vector2f StarGenerator::generateStarPosition()
+{
+    return sf::Vector2f();
+}
+
+sf::Vector2f StarGenerator::generateStarVelocity()
+{
+    return sf::Vector2f();
+}
+
+void StarGenerator::draw(sf::RenderTarget &target)
+{
+}
+
+Star StarGenerator::generateStar(StarSpectralType spectralType)
+{
+    return Star();
+}
+
+
