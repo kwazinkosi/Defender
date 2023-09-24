a6ac680be7243baa3790d216a8b91c8d69015ead
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-24 14:41:55 +0200

    StarGenerator class with other functions defined 2

--- game-source-code/StarGenerator.cpp
+++ game-source-code/StarGenerator.cpp
@@ -34,8 +34,38 @@ void StarGenerator::moveStars(sf::Time deltaTime)
 
 sf::Color StarGenerator::generateStarColor(StarSpectralType spectralType)
 {
-    return sf::Color();
+    // Generate random color
+    sf::Color color;
+    switch (spectralType)
+    {
+    case StarSpectralType::O:
+        color = sf::Color(155, 176, 255); // blue-white
+        break;
+    case StarSpectralType::B:
+        color = sf::Color(170, 193, 255); // blue
+        break;
+    case StarSpectralType::A:
+        color = sf::Color(193, 205, 255); // blue-white
+        break;
+    case StarSpectralType::F:
+        color = sf::Color(226, 232, 255); // blue-white
+        break;
+    case StarSpectralType::G:
+        color = sf::Color(255, 255, 255); // white
+        break;
+    case StarSpectralType::K:
+        color = sf::Color(245, 224, 178); // orange
+        break;
+    case StarSpectralType::M:
+        color = sf::Color(240, 110, 110); // red
+        break;
+    default:
+        color = sf::Color(255, 255, 255); // default to white
+        break;
+    }
+    return color;
 }
+
 
 sf::CircleShape StarGenerator::generateStarSize(float mass)
 {
@@ -58,7 +88,38 @@ sf::Vector2f StarGenerator::generateStarPosition()
 
 sf::Vector2f StarGenerator::generateStarVelocity()
 {
-    return sf::Vector2f();
+    // Generate random velocity
+    int randNumx = rand() % 5;
+    int randNumy = rand() % 5;
+    sf::Vector2f velocity;
+    if (randNumx == 1)
+    {
+        velocity.x = float(rand() % 50 + 50) * -1.f;
+    }
+    else if (randNumx == 0)
+    {
+        velocity.x = float(rand() % 50 + 50);
+    }
+    else
+    {
+        velocity.x = 25.f;
+    }
+
+    if (randNumy == 1)
+    {
+        velocity.y = float(rand() % 10 + 10) * -1.f;
+    }
+    else if (randNumy == 0)
+    {
+        velocity.y = float(rand() % 10 + 10);
+    }
+    else
+    {
+        velocity.y = 0;
+        velocity.x = 0;
+    }
+
+    return velocity;
 }
 
 void StarGenerator::draw(sf::RenderTarget &target)
