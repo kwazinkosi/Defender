2e40239ea516c2a83d74eead57616cca3ffcad59
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-09-25 15:04:26 +0200

    PowerUp class functions and private members declarations

--- game-source-code/PowerUp.cpp
+++ game-source-code/PowerUp.cpp
@@ -7,3 +7,44 @@ PowerUp::PowerUp(Context &context, sf::Vector2f position)
 PowerUp::~PowerUp()
 {
 }
+
+void PowerUp::initPowerUp()
+{
+}
+
+void PowerUp::update(sf::Time deltaTime)
+{
+}
+
+void PowerUp::draw(sf::RenderTarget &target)
+{
+}
+
+bool PowerUp::isStatic() const
+{
+    return false;
+}
+
+ENTITYTYPE PowerUp::getEntityType() const
+{
+    return ENTITYTYPE();
+}
+
+bool PowerUp::collissionCheck(Entity *other)
+{
+    return false;
+}
+
+void PowerUp::onCollision()
+{
+}
+
+bool PowerUp::isDestroyed() const
+{
+    return false;
+}
+
+sf::Vector2f PowerUp::spawnPosition()
+{
+    return sf::Vector2f();
+}
--- game-source-code/PowerUp.hpp
+++ game-source-code/PowerUp.hpp
@@ -17,9 +17,21 @@ class PowerUp: public Entity
     public:
         PowerUp(Context &context, sf::Vector2f position);
         ~PowerUp();
+        void initPowerUp();
+        void update(sf::Time deltaTime);
+        void draw(sf::RenderTarget& target); 
+        bool isStatic() const;
+        ENTITYTYPE getEntityType() const; 
+        bool collissionCheck(Entity* other);
+        void onCollision();id onDestroy();
+        bool isDestroyed() const;
 
     private:
         Context *mContext;
+        sf::Clock mPowerUpClock;
+        sf::Time mPowerUpTime;
+        float mLifeTime;
+        sf::Vector2f spawnPosition();
 
 };
 #endif
