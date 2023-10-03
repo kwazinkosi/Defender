377b8c163018fdbce1069637e73f964a1f3c4193
Kwazinkosi Zwane <kwazinkosizwane@gmail.com>
2023-10-03 12:00:15 +0200

    Updated Animation class interface

--- game-source-code/Animation.hpp
+++ game-source-code/Animation.hpp
@@ -10,23 +10,34 @@
 class Animation
 {
     public:
-        Animation(sf::Texture* texture, sf::Vector2f position, sf::Vector2i frameStart, sf::Vector2i frameSize, std::size_t numFrames, sf::Time duration, bool repeat);
+        Animation() = default; // Default constructor
+        Animation(sf::Texture* texture,sf::Vector2i frameStart, sf::Vector2i frameSize, std::size_t numFrames, sf::Time duration, bool repeat);
         ~Animation();
         void update(sf::Time deltaTime);
         void draw(sf::RenderTarget& target);
+        void move(float x, float y);
+        void setPosition(sf::Vector2f position);
         void setAnimation(sf::Vector2i frameStart, sf::Vector2i frameSize, std::size_t numFrames, sf::Time duration, bool repeat);
+        void setFrameStart(sf::Vector2i frameStart){
+            mFrameStart = frameStart;
+        }
         void setFrameSize(sf::Vector2i frameSize);
         void setRepeat(bool repeat);
         bool isFinished() const;
         void setDuration(sf::Time duration);
         void setNumFrames(std::size_t numFrames);
+        void setScale(float x, float y);
         sf::Sprite& getSprite();
-        void setPosition(sf::Vector2f position);
         sf::Vector2f getPosition() const;
-        sf::IntRect getTextureRect();
-        void move(float x, float y);
+        sf::FloatRect getBounds() const;
 
     private:
+        // Functions
+        void setTextureRectToStart(sf::IntRect& textureRect);   // Set the texture rect to the start of the animation
+        void updateFrameAndTextureRect(sf::IntRect& textureRect, const sf::Vector2i& textureBounds, const sf::Time& timePerFrame);
+        void updateCurrentFrame();
+        // Variables
+        sf::Texture* texture;
         sf::Sprite mSprite;
         sf::Vector2i mFrameStart;
         sf::Vector2i mFrameSize;
