da1f1795e30834b61256bfeb32ba3d51d122c102
Kwazinkosi Zwane <kwazinkosizwane@gmail.com>
2023-10-04 20:32:59 +0200

    Updated Entity class

--- game-source-code/Entity.cpp
+++ game-source-code/Entity.cpp
@@ -1,34 +1,28 @@
 #include "Entity.hpp"
 
 Entity::Entity(int health, float movementSpeed, sf::Vector2f position, ENTITYTYPE entityType)
-: mAnimation(nullptr)
-, mPosition(position)
+: mPosition(position)
 , mEntityType(entityType)
 , mMovementSpeed(movementSpeed)
 , mDestroyed(false)  
 , mHealth(health)
-  
 {
 }
 
 Entity::~Entity()
 {
-    
 }
 
 void Entity::move(float x, float y)
 {
-   sprite.move(x, y);
+    sprite.move(x, y);
+    mPosition.x += x;
+    mPosition.y += y;
 }
 
 void Entity::OnDestroy()
 {
     mDestroyed = true;
-}
-
-void Entity::changeAnimation(sf::Vector2f position, sf::Vector2i frameStart, sf::Vector2i frameSize, std::size_t numFrames, sf::Time duration, bool repeat)
-{
-    mAnimation = std::make_unique<Animation>(&texture, position, frameStart, frameSize, numFrames, duration, repeat);
 }
 
 bool Entity::isDestroyed() const
@@ -41,14 +35,17 @@ void Entity::setHealth(int health)
     mHealth = health;
 }
 
-void Entity::setCollisionType(CollisionType collisionType)
-{
-    mCollisionType = collisionType;
-}
-
 void Entity::setPosition(sf::Vector2f position)
 {
+    sprite.setPosition(position);
     mPosition = position;
+}
+
+void Entity::setPosition(float x, float y)
+{
+    sprite.setPosition(x, y);
+    mPosition.x = x;
+    mPosition.y = y;
 }
 
 void Entity::setMovementSpeed(float movementSpeed)
@@ -61,15 +58,6 @@ int Entity::getHealth() const
     return mHealth;
 }
 
-float Entity::getSpeed() const
-{
-    return mMovementSpeed;
-}
-
-int Entity::getCollisionMask() const
-{
-    return static_cast<int>(mCollisionType);
-}
 sf::FloatRect Entity::getBounds() const
 {
     return sprite.getGlobalBounds();
@@ -82,5 +70,16 @@ sf::Sprite &Entity::getSprite()
 
 sf::Vector2f Entity::getPosition() const
 {
-    return mPosition;
+    //return mPosition;
+    return sprite.getPosition();
+}
+
+sf::Vector2f Entity::getCenter() const
+{
+    return sf::Vector2f(getPosition().x + getBounds().width / 2.f, getPosition().y + getBounds().height / 2.f);
+}
+
+bool Entity::collisionCheck(Entity* other)
+{
+    return getBounds().intersects(other->getBounds());
 }
--- game-source-code/Entity.hpp
+++ game-source-code/Entity.hpp
@@ -11,19 +11,6 @@
 #include "Utills.hpp"
 
 
-// Bitwise enum for colliidable objects
-enum class CollisionType
-{
-    None = 0,
-    Player = 1 << 0,
-    Enemy = 1 << 1,
-    Projectile = 1 << 2,
-    MineBomb = 1 << 3,
-    PowerUp = 1 << 4,
-    Collidable = 1 << 5,
-    Asteroid = 1 << 6,
-    Humanoid = 1 << 7,
-};
 // This is the base class for all entities (player, enemy, etc.) in the game, it contains all the common variables and functions that all entities will use
 class Entity
 {
@@ -31,39 +18,29 @@ class Entity
         explicit Entity(int health, float movementSpeed, sf::Vector2f position, ENTITYTYPE entityType); 
         ~Entity();
 
-        // Functions
-        virtual void update(sf::Time deltaTime) = 0;
-        virtual void draw(sf::RenderTarget& target) = 0;
-        // Check if the collidable object is static( does not move)
-        virtual bool isStatic() const = 0;
+        // // Functions
+        virtual void update(sf::Time deltaTime)=0;
+        virtual void draw(sf::RenderTarget& target)=0;
         virtual ENTITYTYPE getEntityType() const = 0; 
         // Method called when a collision occurs with another collidable object
         virtual void onCollision() = 0;
-        virtual bool collissionCheck(Entity* other) = 0;
+        bool collisionCheck(Entity* other);
+        sf::Vector2f getCenter() const;
         void move(float x, float y);
-        // method called when the entity is destroyed
-        virtual void OnDestroy();
-        // Change the animation of the entity to the specified animation
-        void changeAnimation(sf::Vector2f position, sf::Vector2i frameStart, sf::Vector2i frameSize, std::size_t numFrames, sf::Time duration, bool repeat);
-        
-
-        // Getters and setters
-        bool isDestroyed() const;
-        void setHealth(int health);
-        void setCollisionType(CollisionType collisionType);
         void setPosition(sf::Vector2f position);
-        void setMovementSpeed(float movementSpeed);
-        int getHealth() const;
-        float getSpeed() const;
-        int getCollisionMask() const;
+        void setPosition(float x, float y);
+        sf::Vector2f getPosition() const;
         sf::FloatRect getBounds() const;
         sf::Sprite& getSprite();
-        sf::Vector2f getPosition() const;
-        
+        void OnDestroy();
+        bool isDestroyed() const;
+        void setHealth(int health);
+        int getHealth() const;
+        void setMovementSpeed(float movementSpeed);
+
     protected:
         // Objects
-        CollisionType mCollisionType;
-        std::unique_ptr<Animation> mAnimation;
+        // Variables
         sf::Texture texture;
         sf::Sprite sprite;
         sf::Vector2f mPosition;
@@ -74,5 +51,4 @@ class Entity
         // Variables
         int mHealth;
 };
-
 #endif // ENTITY_HPP
