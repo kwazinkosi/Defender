08ae81fcb2c9617889ad305c1bfeee58c47847ea
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-10-07 17:06:42 +0200

    defining the help state functions v2

--- game-source-code/HelpState.cpp
+++ game-source-code/HelpState.cpp
@@ -56,3 +56,54 @@ bool HelpState::update(sf::Time dt)
 {
     return false;
 }
+bool HelpState::handleEvent(const sf::Event &event, sf::RenderWindow &window)
+{
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::BackSpace)
+    {
+        requestStackPop();
+        requestStackPush(States::MainMenuState);
+    }
+    if (event.type == sf::Event::Closed || (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Enter))
+    {
+        requestStateClear(); // Clear the stack
+        window.close();      // Close the window
+    }
+
+    return false;
+}
+
+void HelpState::handleRealtimeInput(sf::RenderWindow &window)
+{
+}
+
+void HelpState::onActivate() // Called when the state is activated
+{
+    sf::Color backgroundColor(70, 30, 100, 100);
+    backgroundShape.setFillColor(backgroundColor);
+    backgroundShape.setSize(sf::Vector2f(mContext->mWindow.getSize()));
+}
+void HelpState::drawHelp(sf::RenderWindow &window)
+{
+    std::string line;
+    file.open(filePath_, std::ios::in);
+    float ySpacing = 0.35f;
+
+    static float increment = 0.0f;
+    
+    while (std::getline(file, line))
+    {
+
+        std::cout << line << std::endl;
+        sf::Text text;
+        text.setFont(instructionFont);
+        text.setFillColor(sf::Color::White);
+        text.setString(line);
+        text.setCharacterSize(12);
+        text.setPosition(sf::Vector2f(0.05f * mContext->mWindow.getSize().x ,
+                                      ySpacing * mContext->mWindow.getSize().y + increment));
+        window.draw(text);
+        increment += 25.f;
+    }
+    increment = 0.0f;
+    file.close();
+}
