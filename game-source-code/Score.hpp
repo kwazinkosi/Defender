de95f34e638064f124930f6f637b35027e98aa28
Tebogo Dibakoane <tebogodibakoane228@gmail.com>
2023-10-08 18:40:27 +0200

    Generating doxygen comments v7

--- game-source-code/Score.hpp
+++ game-source-code/Score.hpp
@@ -9,16 +9,57 @@
 #include <SFML/System.hpp>
 #include "TextFormater.hpp"
 
+/**
+ * @brief  The score class is responsible for keeping track of the score.
+ * 
+ */
 class Score
 {
 public:
+    /**
+     * @brief Construct a new Score object
+     * 
+     */
     Score();
+    /**
+     * @brief  updates the score.
+     * 
+     * @param type 
+     */
     void updateScore(ENTITYTYPE type);
+    /**
+     * @brief  Draws the score.
+     * 
+     * @param window 
+     */
     void drawScore(sf::RenderWindow& window);
+    /**
+     * @brief  Resets the score.
+     * 
+     */
     void reset();
+    /**
+     * @brief Get the Score object 
+     * 
+     * @return const unsigned int 
+     */
     const unsigned int getScore() const;
+    /**
+     * @brief Get the Score Text object
+     * 
+     * @return sf::Text 
+     */
     sf::Text getScoreText();
+    /**
+     * @brief Set the Score Text object
+     * 
+     * @param font 
+     */
     void setScoreText(const sf::Font& font);
+    /**
+     * @brief  Saves the highscore to a file.
+     * 
+     */
     std::vector<int> mHighscores;
     void appendHighscore(int score);
     void setScore(unsigned int score);
--- game-source-code/ScoreState.hpp
+++ game-source-code/ScoreState.hpp
@@ -6,15 +6,59 @@
 #include "State.hpp"
 #include "TextFormater.hpp"
 
+/**
+ * @brief  The score state of the game.
+ * 
+ */
 class ScoreState : public State
 {
     public:
+        /**
+         * @brief Construct a new Score State object
+         * 
+         * @param stack 
+         * @param context 
+         */
         ScoreState(StateStack& stack, Context &context);
+        /**
+         * @brief  Draw the score state.
+         * 
+         * @param window 
+         */
         virtual void draw(sf::RenderWindow &window);
+        /**
+         * @brief  Update the score state.
+         * 
+         * @param dt 
+         * @return true 
+         * @return false 
+         */
         virtual bool update(sf::Time dt);
+        /**
+         * @brief  Handle an event.
+         * 
+         * @param event 
+         * @param window 
+         * @return true 
+         * @return false 
+         */
         virtual bool handleEvent(const sf::Event &event, sf::RenderWindow &window);
+        /**
+         * @brief  Handle realtime input.
+         * 
+         * @param window 
+         */
         virtual void handleRealtimeInput(sf::RenderWindow &window);
+        /**
+         * @brief Get the State I D object
+         * 
+         * @return std::string 
+         */
         virtual std::string getStateID() const;
+        /**
+         * @brief  Called when the state is activated.
+         * 
+         */
         void onActivate();
     private:
         Context* mContext;
